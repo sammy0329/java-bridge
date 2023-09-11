a1dba23de978c1a87754987fd8c019c902c97ac0
sammy <sammy0329@naver.com>
2023-09-12 01:03:16 +0900

    feat: InputViewTest 클래스 readGameCommand 함수 구현
    InputViewTest 클래스에 게임 재시도 여부 입력 받는 역할은 하는 readGameCommand 함수 구현

--- src/main/java/bridge/View/InputView.java
+++ src/main/java/bridge/View/InputView.java
@@ -29,6 +29,9 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        String gameRetryCommand = camp.nextstep.edu.missionutils.Console.readLine();
+        Validator.validateCorrectRetry(gameRetryCommand);
+
+        return gameRetryCommand;
     }
 }
