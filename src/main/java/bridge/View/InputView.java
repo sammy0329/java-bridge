4c6772a3c8aff96267ab4b5d5895d0c6a3f9e8b2
sammy <sammy0329@naver.com>
2023-09-14 00:36:38 +0900

    refactor: InputView 클래스 함수 수정
    InputView 클래스 함수 내에 System.out.println 추가

--- src/main/java/bridge/View/InputView.java
+++ src/main/java/bridge/View/InputView.java
@@ -9,6 +9,8 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+
         String bridgeSize = camp.nextstep.edu.missionutils.Console.readLine();
         Validator.validateBridgeCnt(bridgeSize);
 
@@ -19,6 +21,8 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+
         String movingCommand = camp.nextstep.edu.missionutils.Console.readLine();
         Validator.validateCorrectMove(movingCommand);
 
@@ -29,6 +33,8 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+
         String gameRetryCommand = camp.nextstep.edu.missionutils.Console.readLine();
         Validator.validateCorrectRetry(gameRetryCommand);
 
