97cb3599aeff16a9be4e01e4ce68d438c8d8b757
sammy <sammy0329@naver.com>
2023-09-14 00:44:36 +0900

    feat: BridgeGame 클래스 구현
    BridgeGame 클래스 싱글톤 패턴 적용
    move : 사용자에게 이동할 칸(U,D)을 입력을 확인하고 이동하도록 하는 함수
    isCorrectMovingCommand : 올바르게 이동 가능할 때, 움직임을 표현해주는 함수
    isNotCorrectMovingCommand : 올바르지 않아 이동하지 못할 때, 움직이지 못함을 표현해주는 함수
    retry : 게임 재실행 여부 판단 함수
    resetBridgeState : 현재 다리 상태 리스트들을 리셋하는 함수
    setAnswerBridge : 만들어진 정답 Bridge를 매개변수로 받아 answerBridge 리스트에 저장시키는 함수
    멤버변수 tryCnt, upBridgeStates, downBridgeStates Getter 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,23 +1,106 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private int tryCnt = 1;
+
+    private int currentBridgeIdx = -1;
+
+    private List<String> upBridgeStates = new ArrayList<>();
+    ;
+    private List<String> downBridgeStates = new ArrayList<>();
+    private List<String> answerBridge;
+
+
+    private static final BridgeGame instance = new BridgeGame();
+
+    private BridgeGame() {}
+
+    public static BridgeGame getInstance() {
+        return instance;
+    }
+
+    public void setAnswerBridge(List<String> getAnswerBridge){
+        answerBridge = getAnswerBridge;
+    }
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(String movingCommand) {
+        currentBridgeIdx++;
+        if (isCorrectMovingCommand(movingCommand)) {
+            return true;
+        }
+        isNotCorrectMovingCommand(movingCommand);
+
+        return false;
+    }
+
+    private boolean isCorrectMovingCommand(String movingCommand) {
+        if (movingCommand.equals("U") && answerBridge.get(currentBridgeIdx).equals(movingCommand)) {
+            upBridgeStates.add("O");
+            downBridgeStates.add(" ");
+            return true;
+        } else if (movingCommand.equals("D") && answerBridge.get(currentBridgeIdx).equals(movingCommand)) {
+            downBridgeStates.add("O");
+            upBridgeStates.add(" ");
+            return true;
+        }
+        return false;
+    }
+
+    private void isNotCorrectMovingCommand(String movingCommand) {
+        if (movingCommand.equals("U") && !answerBridge.get(currentBridgeIdx).equals(movingCommand)) {
+            upBridgeStates.add("X");
+            downBridgeStates.add(" ");
+        } else if (movingCommand.equals("D") && !answerBridge.get(currentBridgeIdx).equals(movingCommand)) {
+            downBridgeStates.add("X");
+            upBridgeStates.add(" ");
+        }
+    }
+
+    public boolean isLastBridge(){
+        return upBridgeStates.size() == answerBridge.size();
     }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * 사용자가 "R" 입력시 true 반환, "Q" 입력시 false 반환
+     * InputView에서 Validator 했기 때문에 넘어오는 값은 "R" or "Q"
      */
-    public void retry() {
+    public boolean retry(String gameRetryCommand) {
+        if (gameRetryCommand.equals("R")) {
+            tryCnt++;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 게임 시작을 위해 upBridgeStates, downBridgeStates 리스트를 초기화 해주는 메서드
+     */
+    public void resetBridgeState() {
+        upBridgeStates.clear();
+        downBridgeStates.clear();
+        currentBridgeIdx = -1;
+    }
+
+    public int getTryCnt() {
+        return tryCnt;
+    }
+
+    public List<String> getUpBridgeStates() {
+        return upBridgeStates;
+    }
+
+    public List<String> getDownBridgeStates() {
+        return downBridgeStates;
     }
 }
